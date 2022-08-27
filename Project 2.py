fd46756e9eda87ea2ae3740b40074c791fd7787e
sameer <106543189+sameeratgitam@users.noreply.github.com>
2022-08-27 14:06:11 +0530

    Add files via upload

---
+++ Project 2.py
@@ -0,0 +1,11 @@
+list1 = [12, -7, 5, 64, -14]
+
+for numbers in list1:
+    if numbers >0:
+        print(numbers)
+
+list2 = [12, 14, -95, 3]
+
+for numbers in list2:
+    if numbers >0:
+        print(numbers)
